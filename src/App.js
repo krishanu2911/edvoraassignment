b5aa54031ec5e733897032f7d54da56867c69590
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-05-14 12:15:25 +0530

    Feat Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import RoutePath from './route/RoutePath';
+import { Navbar } from "./component/index"
 function App() {
   return (
     <div className="App">
-      <h1>nav bar</h1>
+      <Navbar />
       <RoutePath />
     </div>
   );
