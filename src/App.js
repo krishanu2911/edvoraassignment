edf1104d189401d9f2432115e5bfd2e5025609be
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-05-14 12:01:13 +0530

    Feat Routes Defined

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
-
+import RoutePath from './route/RoutePath';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>nav bar</h1>
+      <RoutePath />
     </div>
   );
 }
---
+++ src/pages/NearestRides/NearestRides.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function NearestRides() {
+    return (
+        <div>
+            <h1>Nearest Rides</h1>
+        </div>
+    )
+}
+export default NearestRides;
