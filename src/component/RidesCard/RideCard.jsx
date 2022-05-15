5ea910a800e06a455bf27f77ae5c7650cd86a398
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-05-15 19:29:34 +0530

    Feat Card Design

--- src/component/RidesCard/RideCard.jsx
+++ src/component/RidesCard/RideCard.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-
-function RideCard({mapUrl, id , originStationCode, stationPath, date,distance,state,city }) {
-    return (
-        <div>
-            <img src={mapUrl} />
-              <h3>{id}</h3>
-              <h3>{originStationCode}</h3>
-              <h3>{stationPath}</h3>
-              <h3>{date}</h3>
-              <h3>{distance}</h3>
-              <h3>{state}</h3>
-              <h3>{city}</h3>
+import React from "react";
+import style from "./RideCard.module.css";
+function RideCard({
+  mapUrl,
+  id,
+  originStationCode,
+  stationPath,
+  date,
+  distance,
+  state,
+  city,
+}) {
+  return (
+    <div className={`${style.rideCardContainer} ${style.textColor}`}>
+      <img className={`${style.mapImg}`} src={mapUrl} />
+      <div className={`${style.flex} ${style.rideCardRight}`}>
+        <div className={`${style.textColor} ${style.flexCol} ${style.gap}`}>
+          <h3>Ride id: {id}</h3>
+          <h3>Origin Station: {originStationCode}</h3>
+          <h3>Station path: [{stationPath}]</h3>
+          <h3>Date: {date}</h3>
+          <h3>Distance: {distance}</h3>
         </div>
-    )
+        <div className={`${style.flex} ${style.gap}`}>
+          <div className={`${style.pills}`}>
+            <h4>{state}</h4>
+          </div>
+          <div className={`${style.pills}`}>
+            <h4>{city}</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default RideCard;
