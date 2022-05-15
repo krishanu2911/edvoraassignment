616fec877e6e300bf87325c69799f7eb0974d4af
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-05-15 19:43:50 +0530

    Feat Map RideCard Pages

--- src/pages/FutureRides/FutureRides.jsx
+++ src/pages/FutureRides/FutureRides.jsx
@@ -3,6 +3,7 @@ import { useUserContext } from "../../context/UserContext";
 import { isUpcomingRide } from "../../utils/index";
 import { useFilter } from "../../context/FilterContext";
 import style from "./FutureRides.module.css";
+import { RideCard } from "../../component/index"
 function FutureRides() {
   const [upComingRides, setUpComingRides] = useState([]);
   const { nearestRides } = useUserContext();
@@ -21,8 +22,23 @@ function FutureRides() {
   const filteredUpcomingRides = upComingRides?.filter((item) => (item.state === stateSelected || stateSelected === "state") && ( item.city === citySelected || citySelected === "city" ) );
   console.log(filteredUpcomingRides);
   return (
-    <div>
-      <h1>future Rides</h1>
+    <div className={`${style.rideListingSection}`}>
+      {filteredUpcomingRides?.map((item) => {
+        return (
+          <div>
+            <RideCard
+              mapUrl={item.map_url}
+              id={item.id}
+              originStationCode={item.origin_station_code}
+              stationPath={item.station_path}
+              date={item.date}
+              distance={item.distance}
+              state={item.state}
+              city={item.city}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
